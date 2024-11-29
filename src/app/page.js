39e09b0b45ebe1e0e21adf5187eb08dc1d6013c8
Yoongu Kwon <dbsrn1110@gmail.com>
2024-11-29 11:46:21 +0900

    Footer => 아이콘 구현 및 레이아웃 1 // swiper.scss 파일 생성

--- src/app/page.js
+++ src/app/page.js
@@ -1,18 +1,18 @@
-"use client";
+'use client';
 
-import Footer from "@/components/layout/Footer";
-import Header from "@/components/layout/header/Header";
-import Main from "@/components/layout/Main";
-import Home from "./home/home";
+import Footer from '@/components/layout/Footer';
+import Header from '@/components/layout/header/Header';
+import Main from '@/components/layout/Main';
+import Home from './home/home';
 
 export default function Page() {
-  return (
-    <>
-      <Header />
-      <Main>
-        <Home />
-      </Main>
-      <Footer />
-    </>
-  );
+    return (
+        <>
+            <Header />
+            <Main>
+                <Home />
+            </Main>
+            <Footer />
+        </>
+    );
 }
--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,14 +1,26 @@
-"use client";
+'use client';
 
-import React from 'react'
-import { Container } from '@chakra-ui/react'
+import React from 'react';
+import { Box, Container, Flex } from '@chakra-ui/react';
+import { BookMarked, UserRound } from 'lucide-react';
 
 const Footer = () => {
-  return (
-    <footer className='bg-gray-100 py-6'>
-      <Container>footer</Container>
-    </footer>
-  )
-}
+    return (
+        <footer className='bg-gray-100 fixed bottom-0 inset-x-0'>
+            <Container className='w-full h-14 px-5 py-0'>
+                <Flex justify='space-between' gap='5px' className='h-full'>
+                    <Box className='m-auto'>
+                        <UserRound />
+                    </Box>
+                    <Box className='m-auto'>2</Box>
+                    <Box className='m-auto'>3</Box>
+                    <Box className='m-auto'>
+                        <BookMarked />
+                    </Box>
+                </Flex>
+            </Container>
+        </footer>
+    );
+};
 
-export default Footer
+export default Footer;
