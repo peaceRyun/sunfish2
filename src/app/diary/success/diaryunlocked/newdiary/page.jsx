22753bdbddf0184591108f32da43afc06625995e
Yoongu Kwon <dbsrn1110@gmail.com>
2024-12-03 16:41:29 +0900

    diary => diaryunlocked 페이지 DOM 수정, NewDiary페이지 내용 추가1

--- src/app/diary/success/diaryunlocked/newdiary/page.jsx
+++ src/app/diary/success/diaryunlocked/newdiary/page.jsx
@@ -1,5 +1,56 @@
 'use client';
 
-export default function Home() {
-    return <></>;
+import { Button, Flex, Heading } from '@chakra-ui/react';
+import { useState } from 'react';
+
+export default function NewDiaryPage() {
+    const [title, setTitle] = useState('');
+    const [text, setText] = useState('');
+
+    const handleChangeTitle = (event) => {
+        setTitle(event.target.value);
+    };
+
+    const handleChangeText = (event) => {
+        setText(event.target.value);
+    };
+
+    return (
+        <>
+            <Flex
+                gap='2'
+                direction='column'
+                w='full'
+                minHeight='100vh'
+                p='60px 20px 15px'
+                className='bg-custom-blue-100 rounded'
+            >
+                <Flex justify='space-between'>
+                    <Flex gap='2' align='end'>
+                        <strong className='text-lg leading-none'>02</strong>
+                        <span className='text-xs text-gray-500 leading-none'>12월</span>
+                    </Flex>
+                </Flex>
+
+                <input
+                    type='text'
+                    name='inputName'
+                    placeholder='제목'
+                    className='text-lg font-bold'
+                    value={title}
+                    onChange={handleChangeTitle}
+                ></input>
+                <textarea
+                    name='description'
+                    rows='4'
+                    cols='50'
+                    className='text-gray-600 text-sm'
+                    placeholder='육하원칙(누가, 언제, 어디서, 무엇을, 어떻게, 왜)가 포함되면 좋아요!'
+                    value={text}
+                    onChange={handleChangeText}
+                ></textarea>
+            </Flex>
+            <Button className='absolute top-3 right-5 px-4 rounded text-sm'>저장</Button>
+        </>
+    );
 }
--- src/app/diary/success/diaryunlocked/page.jsx
+++ src/app/diary/success/diaryunlocked/page.jsx
@@ -10,28 +10,26 @@ import Link from 'next/link';
 const DiaryUnlockedPage = () => {
     return (
         <>
-            <Box>
-                <Box pt='50px'>
-                    <Image
-                        src='/images/pattern/main/main-bg1.png'
-                        alt='bg1'
-                        width={390}
-                        height={180}
-                        className='opacity-50'
-                    />
-                </Box>
-                <Flex width='100%' gap='3' direction='column' p='0 20px' className='absolute top-48 left-0'>
-                    <span className='text-xs text-gray-600'>2024</span>
-                    <DiaryCard MoodIcon={Smile} fill='lightgreen' />
-                    <DiaryCard MoodIcon={Angry} fill='tomato' />
-                    <DiaryCard MoodIcon={Frown} fill='#4892E0' />
-                    <DiaryCard MoodIcon={Annoyed} fill='#e0d648' />
-                </Flex>
-                <Box className='fixed bottom-20 left-1/2 -translate-x-1/2'>
-                    <Link href='/diary/success/diaryunlocked/newdiary'>
-                        <CirclePlus color='white' fill='#4892E0' size={50} strokeWidth={1} />
-                    </Link>
-                </Box>
+            <Flex pt='50px' justify='center'>
+                <Image
+                    src='/images/pattern/main/main-bg1.png'
+                    alt='bg1'
+                    width={390}
+                    height={180}
+                    className='opacity-50'
+                />
+            </Flex>
+            <Flex width='100%' gap='3' direction='column' p='0 20px' className='absolute top-48 left-0'>
+                <span className='text-xs text-gray-600'>2024</span>
+                <DiaryCard MoodIcon={Smile} fill='lightgreen' />
+                <DiaryCard MoodIcon={Angry} fill='tomato' />
+                <DiaryCard MoodIcon={Frown} fill='#4892E0' />
+                <DiaryCard MoodIcon={Annoyed} fill='#e0d648' />
+            </Flex>
+            <Box className='fixed bottom-20 left-1/2 -translate-x-1/2'>
+                <Link href='/diary/success/diaryunlocked/newdiary'>
+                    <CirclePlus color='white' fill='#4892E0' size={50} strokeWidth={1} />
+                </Link>
             </Box>
         </>
     );
