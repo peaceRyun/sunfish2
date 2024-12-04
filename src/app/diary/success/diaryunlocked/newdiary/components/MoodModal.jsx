68173c86ea0d15d19805960a09d1ee84dd8a4e3c
Yoongu Kwon <dbsrn1110@gmail.com>
2024-12-04 13:14:33 +0900

    diary => newdiary 모달창 구현2

--- src/app/diary/success/diaryunlocked/newdiary/components/MoodModal.jsx
+++ src/app/diary/success/diaryunlocked/newdiary/components/MoodModal.jsx
@@ -19,32 +19,49 @@ function BasicModal({ MoodIcon = Smile, fill }) {
 
     return (
         <>
-            <Button onClick={onOpen}>
+            <Button variant='plain' onClick={onOpen}>
                 <MoodIcon fill={fill} />
             </Button>
 
-            <Modal isOpen={isOpen} onClose={onClose}>
+            <Modal isOpen={isOpen} onClose={onClose} size='xs'>
                 <ModalOverlay />
-                <ModalContent>
-                    <ModalHeader className='text-md'>오늘의 기분</ModalHeader>
-                    <ModalCloseButton />
+                <ModalContent className='relative w-64 bg-white rounded-2xl shadow-lg'>
+                    <div
+                        className='absolute w-4 h-4 bg-white transform rotate-45'
+                        style={{
+                            top: '-8px',
+                            right: '16px',
+                        }}
+                    />
+                    <ModalHeader className='text-sm pt-3 pb-2 px-4'>오늘의 기분</ModalHeader>
+                    <ModalCloseButton className='top-2 right-2' />
 
-                    <ModalBody>
-                        <Grid templateRows='repeat(1, 1fr)' templateColumns='repeat(5, 1fr)'>
-                            <GridItem mx='auto'>
-                                <Smile fill='lightgreen' />
+                    <ModalBody className='p-2'>
+                        <Grid templateRows='repeat(1, 1fr)' templateColumns='repeat(5, 1fr)' gap={1}>
+                            <GridItem className='text-center'>
+                                <Button variant='plain' className='p-0 hover:bg-gray-100 rounded-full'>
+                                    <Smile fill='lightgreen' width='36px' height='36px' />
+                                </Button>
                             </GridItem>
-                            <GridItem mx='auto'>
-                                <Angry fill='tomato' />
+                            <GridItem className='text-center'>
+                                <Button variant='plain' className='p-0 hover:bg-gray-100 rounded-full'>
+                                    <Angry fill='tomato' width='36px' height='36px' />
+                                </Button>
                             </GridItem>
-                            <GridItem mx='auto'>
-                                <Frown fill='#4892E0' />
+                            <GridItem className='text-center'>
+                                <Button variant='plain' className='p-0 hover:bg-gray-100 rounded-full'>
+                                    <Frown fill='#4892E0' width='36px' height='36px' />
+                                </Button>
                             </GridItem>
-                            <GridItem mx='auto'>
-                                <Annoyed fill='#e0d648' />
+                            <GridItem className='text-center'>
+                                <Button variant='plain' className='p-0 hover:bg-gray-100 rounded-full'>
+                                    <Annoyed fill='#e0d648' width='36px' height='36px' />
+                                </Button>
                             </GridItem>
-                            <GridItem mx='auto'>
-                                <Meh fill='lightgray' />
+                            <GridItem className='text-center'>
+                                <Button variant='plain' className='p-0 hover:bg-gray-100 rounded-full'>
+                                    <Meh fill='lightgray' width='36px' height='36px' />
+                                </Button>
                             </GridItem>
                         </Grid>
                     </ModalBody>
