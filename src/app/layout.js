31722e9bdb7c0ffd28c7ae57da7912074170941e
Yoongu Kwon <dbsrn1110@gmail.com>
2024-12-06 17:28:58 +0900

    문서 이름 변경

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,7 +10,7 @@ export default function RootLayout({ children }) {
             <head>
                 <meta charSet='UTF-8' />
                 <meta name='viewport' content='width=device-width, initial-scale=1.0' />
-                <title>next.js themplate</title>
+                <title>sunfish 프로젝트</title>
                 <meta property='og:type' content='website' />
                 <meta property='og:title' content='페이지 제목' />
                 <meta property='og:description' content='페이지 설명' />
