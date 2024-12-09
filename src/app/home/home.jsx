011e54c67136c3eaf687d181933c03b7e32cf2e6
Yoongu Kwon <dbsrn1110@gmail.com>
2024-12-09 17:40:51 +0900

    animate 패키지 설치 // home => 디스플레이 문구 추가

--- src/app/home/home.jsx
+++ src/app/home/home.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import Menu from './menu';
-import { Flex } from '@chakra-ui/react';
+import { Flex, Heading } from '@chakra-ui/react';
 import { LedBoardDaily } from './LedBoard';
+import 'animate.css';
 
 const Home = () => {
     return (
@@ -18,6 +19,9 @@ const Home = () => {
                         <LedBoardDaily />
                     </Flex>
                     <Menu p='30px 0px 30px'></Menu>
+                    <Heading as='h2' size='lg' className='animate__animated animate__flip font-custom2'>
+                        스트레스를 날려봅시다!
+                    </Heading>
                 </Flex>
             </div>
         </>
