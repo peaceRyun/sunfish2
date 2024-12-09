649e4331b0c994c2883c5bec6a3a36ae01ef3561
Yoongu Kwon <dbsrn1110@gmail.com>
2024-12-09 15:31:17 +0900

    마이너 이슈 수정2('보수중' 화면 추가)

--- src/app/entertain/page.jsx
+++ src/app/entertain/page.jsx
@@ -1,7 +1,18 @@
+import Image from 'next/image';
 import React from 'react';
 
 const EntertainPage = () => {
-    return <div>entertain</div>;
+    return (
+        <>
+            <Image
+                src='/images/pattern/fixing.jpg'
+                alt='fixing'
+                width={1000}
+                height={1000}
+                className='w-full m-auto'
+            ></Image>
+        </>
+    );
 };
 
 export default EntertainPage;
