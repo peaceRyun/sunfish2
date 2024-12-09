4af09250cc8a7cb3f5bfbf4e71225adae3ac03e4
Yoongu Kwon <dbsrn1110@gmail.com>
2024-12-09 17:43:44 +0900

    footer => 반응형 수정

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -65,7 +65,7 @@ const navigation = [
 export default function Footer() {
     return (
         <footer className='bg-white'>
-            <div className='mx-auto max-w-7xl px-6 py-12 md:flex md:items-center md:justify-between lg:px-8'>
+            <div className='mx-auto max-w-7xl px-6 py-12'>
                 <div className='flex justify-center gap-x-6 md:order-2'>
                     {navigation.map((item) => (
                         <a key={item.name} href={item.href} className='text-gray-600 hover:text-gray-800'>
