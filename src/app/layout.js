0aac239641bd8a6f1db84e7dbf39c1dbb0ad27da
Yoongu Kwon <dbsrn1110@gmail.com>
2024-12-10 17:27:37 +0900

    metadata 분리

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,31 +1,7 @@
-'use client';
-
 import '@/styles/globals.scss';
 import { Providers } from '@/components/providers';
 import SkipNav from '@/components/layout/header/SkipNav';
-
-export const metadata = {
-    title: 'next.js template',
-    description: '페이지 설명',
-    openGraph: {
-        title: '페이지 제목',
-        description: '페이지 설명',
-        type: 'website',
-        url: 'http://www.mysite.com/article/article1.html',
-        images: [
-            {
-                url: 'http://www.mysite.com/article/article1_featured_image.jpg',
-            },
-        ],
-    },
-    twitter: {
-        card: 'summary',
-        title: '페이지 제목',
-        description: '페이지 설명',
-        images: ['http://www.mysite.com/article/article1.html'],
-        creator: '사이트 명',
-    },
-};
+import { metadata } from './metadata';
 
 export default function RootLayout({ children }) {
     return (
---
+++ src/app/metadata.js
@@ -0,0 +1,22 @@
+export const metadata = {
+    title: 'next.js template',
+    description: '페이지 설명',
+    openGraph: {
+        title: '페이지 제목',
+        description: '페이지 설명',
+        type: 'website',
+        url: 'http://www.mysite.com/article/article1.html',
+        images: [
+            {
+                url: 'http://www.mysite.com/article/article1_featured_image.jpg',
+            },
+        ],
+    },
+    twitter: {
+        card: 'summary',
+        title: '페이지 제목',
+        description: '페이지 설명',
+        images: ['http://www.mysite.com/article/article1.html'],
+        creator: '사이트 명',
+    },
+};
